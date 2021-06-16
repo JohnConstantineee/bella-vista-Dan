ecf34664fe163205db5141159536afb2267b1066
Никита Голуб <sleipnir1148@gmail.com>
2021-06-16 20:58:05 +0300

    fix4,6

--- classworkJava/js/script.js
+++ classworkJava/js/script.js
@@ -1,1 +1,14 @@
-console.log("Gogi")
+let name = prompt('Give me your name')
+let age = prompt('Give your age')
+
+console.log(name, age)
+
+ if(name==='') {
+console.error('Wrong name');
+ }
+
+ if(isNaN(age)) {
+     console.error('Wrong age')
+ } else if (+age < 0 ) {
+     console.error('Age less then zero')
+ }
