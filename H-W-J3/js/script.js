e7df5afb3ba8ee905676bf7d62385445e81afaa7
Никита Голуб <sleipnir1148@gmail.com>
2021-07-06 14:15:27 +0300

    money

--- H-W-J3/js/script.js
+++ H-W-J3/js/script.js
@@ -1,10 +1,19 @@
+
 let numFirst = +prompt('Введите первое число');
 let numSecond = +prompt('Введите второе число');
 let operator = prompt('Введите знак операции');
 
-function calc(numFirst, numSecond, operator) {
-    return eval(`${numFirst}` + operator + `${numSecond}`);
+function calc (numFirst, numSecond, operator) {
+    switch (operator) {
+        case '+':
+            return numFirst + numSecond;
+        case '-':
+            return numFirst - numSecond;
+        case '*':
+            return numFirst * numSecond;
+        case '/':
+            return numFirst / numSecond;
+    }
 }
-
-alert(calc(numFirst, numSecond, operator));
+alert(calc(numFirst,numSecond,operator));
 console.log(calc(numFirst,numSecond,operator));
