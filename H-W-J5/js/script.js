35013ad7fd57d4eaf80f266ea4c6ae16c95c0dc1
Никита Голуб <sleipnir1148@gmail.com>
2021-07-15 14:00:15 +0300

    x

--- H-W-J5/js/script.js
+++ H-W-J5/js/script.js
@@ -1,23 +1,24 @@
+
 function createNewUser() {
     return {
-        firstName: prompt('Введите имя '),
-        lastName: prompt('Введите фамилию'),
-        dateBirth: +prompt('Введите дату рождения в формате dd.mm.yyyy'),
-        getLogin () {
+        firstName: prompt('Enter your name'),
+        lastName: prompt('Enter your lastname'),
+        dateBirth: prompt('Enter your birthday (dd.mm.yyyy)'),
+        getLogin() {
             return (this.firstName[0] + this.lastName).toLowerCase();
         },
-        getAge () {
+        getAge() {
             const now = Date.now();
-            const year = this.dateBirth.slice(6, 10);
-            const month = this.dateBirth.slice(3, 5);
-            const day = this.dateBirth.slice(0, 2);
-            const changeDateBirth = new Date(`${year},${month},${day}`);
+            const year = this.dateBirth.slice(6,10);
+            const month = this.dateBirth.slice(3,5);
+            const day = this.dateBirth.slice(0,2);
+            const changeDateBirth = new Date(`${year},${month},${day}`)
             const calcDateBirth = now - changeDateBirth;
-            return new Date(calcDateBirth). getFullYear() -1970;
+            return new Date(calcDateBirth).getFullYear() - 1970;
         },
-        getPassword () {
-            return (this.firstName[0] + (this.lastName.toLowerCase()) + this.dateBirth.getFullYear());
-        },
+        getPassword() {
+            return this.firstName[0].toUpperCase() + this.lastName.toLowerCase() + this.dateBirth.slice(6, 10)
+        }
     }
 }
 const newUser = createNewUser();
