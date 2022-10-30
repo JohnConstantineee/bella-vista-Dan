1910e65164f04b4f9cdc48f968fe59975f96d0c8
Никита Голуб <sleipnir1148@gmail.com>
2022-10-30 20:03:44 +0200

    home

--- HW2N/src/js/script.js
+++ HW2N/src/js/script.js
@@ -1,9 +1,8 @@
-
 const burgerButton = document.querySelector('.header__burger-button');
 const burgerMenu = document.querySelector('.header__burger-menu');
 burgerButton.addEventListener('click', e => {
     burgerMenu.classList.toggle('header__burger-menu--active')
-
+    burgerButton.classList.toggle('active-burger')
 })
 
-
+console.log('')
